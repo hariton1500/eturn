1f823219b069860187aa4f3a733f49c98db4ab08
hariton1500 <hariton1500@gmail.com>
2026-01-30 12:02:56 +0300

    9

--- lib/Screens/station.dart
+++ lib/Screens/station.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:eturn/funcs.dart';
 import 'package:eturn/globals.dart';
 import 'package:eturn/models/socket.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +14,8 @@ class StationScreen extends StatefulWidget {
 class _StationScreenState extends State<StationScreen> {
 
   late StreamSubscription sub;
-  Map<String, dynamic> sending = {'category': 'station'}, state = {};
+  Map<String, dynamic> sending = {'category': 'station'};
+  List<Map<String, dynamic>> state = [], fractions = [], playerShips = [];
 
   @override
   void initState() {
@@ -24,6 +26,8 @@ class _StationScreenState extends State<StationScreen> {
         loadFromDB(event['data']);
       }
     });
+
+    
 
     //sending data request
     sending['type'] = 'get_state';
@@ -41,21 +45,71 @@ class _StationScreenState extends State<StationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Text('Money: ${state['money'] ?? 0} isk'),
+        leading: Text('Money: 0 isk'),
       ),
       body: SafeArea(
         child: Center(
-          child: Text(state.toString()),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ...fractions.map((f) => Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.black
+                      )
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(f['name']),
+                    )
+                  )),
+                  //if ((state.firstWhere((s) => s['fraction_id'] == f['id'])).isNotEmpty) ElevatedButton(onPressed: () {}, child: Text('Take First Ship'))
+                ],
+              ),
+              showHangar()
+            ],
+          ),
         )
       ),
     );
   }
 
   void loadFromDB(Map<String, dynamic> data) async {
-    List<Map<String, dynamic>> request = await sb.from('players_progress').select().eq('player_id', data['player_id']);
-    print(request);
+    //load fractions list
+    printD('requesting fraction');
+    List<Map<String, dynamic>> fractionRequest = await sb.from('fractions').select();
+    printD(fractionRequest.toString());
     setState(() {
-      state = request[0];
+      fractions = fractionRequest;
     });
+
+    printD('requesting players_progress');
+    List<Map<String, dynamic>> request = await sb.from('players_progress').select().eq('email', data['player_id']);
+    printD(request.toString());
+    if (request.isNotEmpty) {
+      setState(() {
+        state = request;
+      });
+    }
+
+    printD('requesting player_ships');
+    List<Map<String, dynamic>> playerShipsRequest = await sb.from('player_ships').select().eq('email', data['player_id']);
+    printD(playerShipsRequest.toString());
+    setState(() {
+      playerShips = playerShipsRequest;
+    });
+
+
+  }
+  
+  Widget showHangar() {
+    return Wrap(
+      children: [
+        ...playerShips.map((ship) => Text(ship['ship_id']))
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   Supabase.initialize(url: dotenv.get('url', fallback: ''), anonKey: dotenv.get('key', fallback: ''));
   final socket = SocketService();
-  socket.connect('ws://192.168.10.75:8080');
+  //socket.connect('ws://192.168.10.75:8080');
+  socket.connect('ws://localhost:8080');
   runApp(const MainApp());
 }
 
